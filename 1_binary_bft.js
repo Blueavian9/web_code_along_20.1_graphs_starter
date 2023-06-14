263102f5990e39b8fa59c12834e346fabfcf27a1
Cesar Aguilar <Blueavian9@gmail.com>
2023-06-14 10:15:16 -0700

    Initial commit

--- 1_binary_bft.js
+++ 1_binary_bft.js
@@ -9,12 +9,23 @@ class node {
 function traverse(root) {
   const values = []
   const queue = []
-
-  // your BFT code here
-
+ // your BFT code here
+  queue.push(root) 
+  while(queue.length) {
+    // Dequeue: queue.shift()
+    // dequeue the value from the queue and push it to the [] output: 
+    let current = queue.shift();
+    values.push(current.value)
   // Enqueue: queue.push(value)
-
-  // Dequeue: queue.shift()
+  // enqueue the children of the value that is being dequed
+    if (current.left){
+      queue.push(current.left)
+    }
+    if (current.right){
+      queue.push(current.right)
+    }
+  }
+  return values  
 }
 
 // Implement a breadth-first traversal (BFT) for a binary tree.
@@ -42,6 +53,38 @@ BFS output: [6, 3, 9,2,4,10]
 console.log(traverse(head)) // [6, 3, 9]
 
 
-//BFT = Level Order Traversal why? beacuse its 1 level at a time
+//BFT = Level Order Traversal why? beacuse it is 1 level at a time.
 
 //DFT = 
+
+    // expected output: =>[1,2,3,4,5,6,7]
+    //       1
+    //     /   \
+    //    2     3
+    //  /  \   / \
+    // 4    5 6   7
+    //  1) dequeue the value from the queue and push it to the [] output: 
+    //  2) enqueue the children of the value that is being dequed
+
+    // queue: []
+    // BFS output: [1,2,3,4,5,6,7]
+    //
+    // Bredth First Traversal = Level Order Traversal why? becuase it's 1 level at a time.
+    // Queue: First In First Out "FIFO": 
+    // stack: Last In First Out "LIFO":
+    //
+    //
+
+    // Test Casees 2: 
+    //   const head = new node()
+    //   head.left = new node() 
+    //   head.right = new node()
+    // // Depth First Traversal  = 
+    // console.log(traverse(head)) // []
+
+
+   // 0 equals false
+   // any other number will be true
+  // if(1) {
+  //   console.log('hey!');
+  // }
